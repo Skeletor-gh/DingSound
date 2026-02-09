eb05cfc502649c3dba8838147a0fbb8331b392dd
Skeletor <jmdsmg@gmail.com>
2026-02-09 21:04:09 +0000

    Add default sound mute toggle and separate sound list file

---
+++ DingSound_SoundList.lua
@@ -0,0 +1,12 @@
+-- DingSound sound list
+--
+-- Add one filename per line inside the table below.
+-- Files must exist in: Interface/AddOns/DingSound/Sounds/
+--
+-- Examples:
+--   "levelup.mp3",
+--   "fanfare.ogg",
+
+DingSoundSoundList = {
+    "FF7_victory.mp3", -- Final Fantasy 7 fanfare
+}
