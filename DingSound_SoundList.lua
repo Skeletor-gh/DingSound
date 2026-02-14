1e5d444f7d31412d949573460959351a3e29b8c1
Skeletor <jmdsmg@gmail.com>
2026-02-14 19:51:47 +0000

    version 1.04r

--- DingSound_SoundList.lua
+++ DingSound_SoundList.lua
@@ -9,4 +9,8 @@
 
 DingSoundSoundList = {
     "FF7_victory.mp3", -- Final Fantasy 7 fanfare
+	"FNV_ding.mp3", -- Fallout New Vegas Ding
+	"GTA_SA.mp3", -- GTA San Andreas Mission Complete Sound
+	"Zelda_Chest.mp3", -- Legend of Zelda Chest Sound
+	"Skyrim_ding.mp3", -- Skyrim Level Up Sound
 }
